f6013bcd22970ba3e17921501e07bd1698e6f0f3
weathon <wguo6358@gmail.com>
2024-11-16 18:32:01 -0800

    added dual stream

---
+++ dual_stream.py
@@ -0,0 +1,46 @@
+from transformers.models.segformer import TwoStreamSegformerEncoder, SegformerConfig, TwoStreamSegformerModel, TwoStreamSegformerForSemanticSegmentation
+import torch
+import numpy as np
+from PIL import Image
+
+
+
+class MyModel(torch.nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        config = SegformerConfig.from_pretrained("nvidia/segformer-b2-finetuned-ade-512-512")
+        config.hidden_dropout_prob = args.hidden_dropout_prob
+        config.attention_probs_dropout_prob = args.attention_probs_dropout_prob
+        config.drop_path_rate = args.drop_path_rate
+        config.classifier_dropout = args.classifier_dropout
+        self.backbone = TwoStreamSegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b2-finetuned-ade-512-512", config=config)
+        self.backbone.decode_head.classifier = torch.nn.Identity() # need this!!!! was not able to train
+        
+        self.proj = torch.nn.Sequential(
+            torch.nn.Dropout2d(0.1), 
+            torch.nn.Conv2d(256, 1, kernel_size=(1 ,1), padding="same")
+        )
+            
+    
+    def forward(self, X):
+        in_img, long_img, short_img = X[:, :3], X[:, 3:6], X[:, 6:]
+        pred = self.backbone(in_img, long_img, short_img).logits 
+        pred = torch.nn.functional.interpolate(pred, size=(512, 512), mode='nearest')
+        pred = self.proj(pred)
+        return pred
+    
+if __name__ == '__main__':
+    class Args:
+        def __init__(self):
+            self.attention_probs_dropout_prob = 0.1
+            self.hidden_dropout_prob = 0.1
+            self.drop_path_rate = 0.1
+            self.classifier_dropout = 0.1
+            self.ksteps = 200 
+
+
+    model = MyModel(Args())
+    model.eval()
+    input_tensor = torch.rand(1, 9, 512, 512)
+    output = model(input_tensor)
+    print(output.shape)
