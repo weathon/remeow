2570be74b368d280b6eb3ceba6c33a557ba9cf07
weathon <wguo6358@gmail.com>
2024-11-30 02:03:25 -0800

    remove inplace

--- mini_unet.py
+++ mini_unet.py
@@ -9,18 +9,18 @@ class MiniUNet(nn.Module):
         # Encoder
         self.enc1 = nn.Sequential(
             nn.Conv2d(in_channels, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Dropout2d(0.2)
         )
         self.pool1 = nn.MaxPool2d(2)
         
         self.enc2 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Dropout2d(0.2)
         )
         self.pool2 = nn.MaxPool2d(2)
@@ -28,9 +28,9 @@ class MiniUNet(nn.Module):
         # Bottleneck
         self.bottleneck = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Dropout2d(0.2)
         )
         
@@ -38,18 +38,18 @@ class MiniUNet(nn.Module):
         self.up1 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
         self.dec1 = nn.Sequential(
             nn.Conv2d(256, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Dropout2d(0.2)
         )
         
         self.up2 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
         self.dec2 = nn.Sequential(
             nn.Conv2d(128, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Dropout2d(0.2)
         )
         
