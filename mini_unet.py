90774b1ac14024bc732e3c3b2553c8793cf9d95e
weathon <wguo6358@gmail.com>
2024-12-01 19:28:51 -0800

    pixel shuffle unet

--- mini_unet.py
+++ mini_unet.py
@@ -29,13 +29,23 @@ class MiniUNet(nn.Module):
         )
         
         # Decoder
-        self.up1 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        # self.up1 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.up1 = nn.Sequential(
+            nn.PixelShuffle(2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+        )
         self.dec1 = nn.Sequential(
             nn.Conv2d(256, 128, kernel_size=3, padding=1),
             nn.ReLU(),
         )
         
-        self.up2 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        # self.up2 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.up2 = nn.Sequential(
+            nn.PixelShuffle(2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+        )
         self.dec2 = nn.Sequential(
             nn.Conv2d(128, 64, kernel_size=3, padding=1),
             nn.ReLU(),
